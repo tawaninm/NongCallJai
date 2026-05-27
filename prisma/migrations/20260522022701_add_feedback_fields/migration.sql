b3ef2b80d88313ab95b9422cde7c878c11312e80
Your Name <your@email.com>
2026-05-27 19:32:52 +0700

    chore: remove prisma folder (migrated to mongoose)

--- prisma/migrations/20260522022701_add_feedback_fields/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "CallFeedbackLog" ADD COLUMN     "caring_message" TEXT,
-ADD COLUMN     "meal" BOOLEAN,
-ADD COLUMN     "meal_detail" TEXT,
-ADD COLUMN     "medication_detail" TEXT,
-ADD COLUMN     "medication_status" BOOLEAN,
-ADD COLUMN     "today_activity" TEXT;
